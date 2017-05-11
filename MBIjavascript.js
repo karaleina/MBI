ad318d109ad41b1c6a1f459e13022960bd790e0a
Karolina Walędzik <karaleina@gmail.com>
2017-05-11 20:06:28 +0200

    Added getting data from the user input.

--- MBIjavascript.js
+++ MBIjavascript.js
@@ -6,19 +6,26 @@ $(document).ready(function(){
 
     $("#deleteDataTable").on('click', function() {
         $('#input_data_table').find("tr").remove();
+        $('#input_data_table').addClass('hidden')
+                              .removeClass('visible');
     });
 
     $("#createDataTable").on('click', function(){
+
+        $('#input_data_table').find("tr").remove();
 
         //pobieranie wymiarów m i n (liczby cech i liczby próbek)
         var wymiar_m = getM();
         var wymiar_n = getN();
 
+        $('#input_data_table').removeClass('hidden')
+                              .addClass('visible');
         //tworzenie danych wierszy
         var tableNormalRowData = "";
         var tableHeaderRowData = "<th></th>"
         for (var i = 0; i < wymiar_m ; i++ ){
-            tableNormalRowData += "<td contenteditable ='true'></td>";
+            var index = i + 1;
+            tableNormalRowData += "<td contenteditable='true' class=M" + index + "></td>";
             if (i > 0) {
                 tableHeaderRowData += "<th>" + "M" + i + "</th>";
             }
@@ -38,29 +45,52 @@ $(document).ready(function(){
                 $('#input_data_table').append(tableHeaderRow);
             }
             else {
-                tableRow = "<tr>" + "<th>" + "N" + j + "</th>" + tableNormalRowData + "</tr>";
+                tableRow = "<tr class=" + "N" + j +">" + "<th>" + "N" + j + "</th>" + tableNormalRowData + "</tr>";
                 console.log(tableRow);
                 $('#input_data_table').append(tableRow);
             }
 
         }
 
-        tableRow = "<tr>" + "<th>" + "N" + wymiar_n +"</th>" + tableNormalRowData + "</tr>";
+        tableRow = "<tr class=" + "N" + wymiar_n +">" + "<th>" + "N" + wymiar_n +"</th>" + tableNormalRowData + "</tr>";
         console.log(tableRow);
         $('#input_data_table').append(tableRow);
 
+    });
 
-        function getM(){
-          var wymiar_m = $("#wymiar_m").val();
-          console.log("Wybrana liczba m cech = " + wymiar_m);
-          return wymiar_m;
-        }
-        function getN(){
-          var wymiar_n = $("#wymiar_n").val();
-          console.log("Wybrana liczba n cech = " + wymiar_n);
-          return wymiar_n;
-        }
+    function getM(){
+      var wymiar_m = $("#wymiar_m").val();
+      console.log("Wybrana liczba m cech = " + wymiar_m);
+      return wymiar_m;
+    }
+    function getN(){
+      var wymiar_n = $("#wymiar_n").val();
+      console.log("Wybrana liczba n cech = " + wymiar_n);
+      return wymiar_n;
+    }
 
-      });
+    $("#getData").on("click", function(){
+        var dataMatrix = [];
+        var m = getM();
+        var n = getN();
+
+        console.log("Rozpoczęto wczytywanie danych...");
+
+        var currentValue = 0;
+        var currentRow = 0;
+        var dataCurrentRow = [];
+
+        for( var i = 1; i <= n; i++){
+            dataCurrentRow=[];
+            currentRow = $('#input_data_table').find(".N" + i);
+            for (var j = 1; j<= m; j++){
+                currentValue = currentRow.find(".M" + j).text();
+                dataCurrentRow.push(currentValue);
+            }
+            dataMatrix.push("[" + dataCurrentRow + "]");
+        }
+        console.log("Odczytane dane: " + dataMatrix);
+    });
+
 
   });
